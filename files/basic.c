6a581a437edff8bbc36c3bc9251c520bd93e1a3d
Aizzzen <gadamurrr@gmail.com>
2024-05-29 21:11:38 +0300

    feat: added some notes and done with C

---
+++ files/basic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main() {
+    // // запись
+    // FILE* fp = fopen("text.txt", "w");
+
+    // char str[] = "Yunus Gadamurov";
+
+    // for(int i = 0; i < str[i]; ++i) {
+    //     fputc(str[i], fp);
+    // }
+
+    // // чтение
+    // FILE* fp = fopen("text.txt", "r");
+
+    // char buff[100];
+    // char ch;
+    // int i = 0;
+    // while((ch = fgetc(fp)) != EOF) {
+    //     buff[i++] = ch;
+    // }
+    // buff[i] = '\0';
+    // puts(buff);
+
+    // fclose(fp);
+
+    return 0;
+}
