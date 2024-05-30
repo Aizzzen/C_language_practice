a00fa6bac0f00bf3b27b931ce809fd72a4963b4d
Aizzzen <gadamurrr@gmail.com>
2024-05-31 02:01:31 +0300

    feat: added cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 
+
+namespace firstSpace {
+void foo() {
+    std::cout << "Hello from space 1" << std::endl;
+}
+}
+
+namespace secondSpace {
+void foo() {
+    std::cout << "Hello from space 2" << std::endl;
+}
+}
+
 int main() {
+    firstSpace::foo();
+    secondSpace::foo();
+
     // std::cout << "Hello world" << std::endl;
 
-    char str[] = "Yunus Gadamurov";
-    short age = 21;
-    double weight = 88;
+    // char str[] = "Yunus Gadamurov";
+    // short age = 21;
+    // double weight = 88;
 
-    // std::cin >> str; // до первого пробельного символа
-    // getline(cin, str);
+    // // std::cin >> str; // до первого пробельного символа
+    // // getline(cin, str);
 
-    std::cin >> age >> weight;
-    std::cout << str << "\n" << age << '\n' << weight << std::endl;
-    return 0;
+    // std::cin >> age >> weight;
+    // std::cout << str << "\n" << age << '\n' << weight << std::endl;
+    // return 0;
 }
